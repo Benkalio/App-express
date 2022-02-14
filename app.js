eaab38d99ad015220a1458958d9e8a52e272f1c6
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-02-15 00:12:06 +0100

    sessions

--- app.js
+++ app.js
@@ -41,40 +41,40 @@ app.use(session({
   store: new FileStore()
 }))
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
   
   if(!req.session.user) {
-    var authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-      var err = new Error('You need to be authenticated');
+    var err = new Error('You need to be authenticated');
 
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-  
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  
-    var username = auth[0];
-    var password = auth[1];
-  
-    if (username === 'admin' && password === 'password') {
-      //COOKIE SETUP
-      // res.cookie('user', 'admin', { signed: true })
-      req.session.user = 'admin';
-      next();
-    } 
-    else {
-      var err = new Error('You need to be authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
   }
+    // var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  
+    // var username = auth[0];
+    // var password = auth[1];
+  // else {
+  //   if (username === 'admin' && password === 'password') {
+  //     //COOKIE SETUP
+  //     // res.cookie('user', 'admin', { signed: true })
+  //     req.session.user = 'admin';
+  //     next();
+  //   } 
+  //   else {
+  //     var err = new Error('You need to be authenticated');
+  //     res.setHeader('WWW-Authenticate', 'Basic');
+  //     err.status = 401;
+  //     return next(err);
+  //   }
+  // }
   else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
@@ -91,8 +91,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter)
